cfb037d2c3149d0d329f528d2de925c2cac94e96
ethanrp2 <ethanrp2@illinois.edu>
2022-12-01 19:37:41 -0600

    integrated motor class methods into forces class

--- Simulation/6DOF_RK4/forces.py
+++ Simulation/6DOF_RK4/forces.py
@@ -14,33 +14,35 @@ import numpy as np
 
 class Forces:
     atm = atmosphere.Atmosphere()
+    motor = motor.Motor()
 
     def __init__(self):
         pass
 
-    def get_force(self, x_state, flap_ext, tStamp):
-        return motor.get_thrust + self.aerodynamic_drag_force(x_state, flap_ext) + self.gravitational_force(x_state[0]) + self.wind_force()
+    def get_force(self, x_state, flap_ext, tStamp) -> float:
+        return self.motor.get_thrust(tStamp) + self.aerodynamic_drag_force(x_state, flap_ext) + self.gravitational_force(x_state[0], tStamp) + self.wind_force()
 
-    def get_Cd(flap_ext):
-        # account for area change of flaps
-        return 1
+    def get_Cd(flap_ext) -> float:
+        # TODO: account for area change of flaps in C_d calculation
+        return prop.C_d
     
-    def aerodynamic_drag_force(self, x_state, flap_ext):
+    def aerodynamic_drag_force(self, x_state, flap_ext) -> float:
         z = x_state[0]
         vel = x_state[1]
         v_mag = np.norm(vel)
         density = self.atm.get_density(z)
-        return -(vel/v_mag) * 0.5*density*(v_mag**2)*prop.C_d*(prop.A)
+        return -(vel/v_mag) * 0.5*density*(v_mag**2)*self.get_Cd(flap_ext)*(prop.A)
     
-    def gravitational_force(altitude):
+    def gravitational_force(self, altitude, tStamp) -> float:
         # F = GMm/r^2
-        return -np.array(0, [(prop.G*prop.m_e*prop.mass)/((prop.r_e+altitude)**2)], 0)
+        total_mass = prop.mass + self.motor.get_mass(tStamp) #Adding dry mass + motor mass
+        return -np.array(0, [(prop.G*prop.m_e*total_mass)/((prop.r_e+altitude)**2)], 0)
     
-    def wind_force(self, altitude, tStamp):
+    def wind_force(self, altitude, tStamp) -> float:
         wind_vector = self.atm.get_wind_vector(tStamp)
         density = self.atm.get_density(altitude)
         wind_vector_mag = wind_vector.norm()
-        # Change C_d to C_d from the side for wind
+        # TODO: Change C_d to C_d from the side for wind
         return (wind_vector/wind_vector_mag) * 0.5*density*(wind_vector_mag**2)*prop.C_d*(prop.A_s)
 
 
