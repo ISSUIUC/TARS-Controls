0768789235b969ab9554b84d5dfe9a2f36e6398e
ethanrp2 <ethanrp2@illinois.edu>
2022-12-01 19:53:51 -0600

    small changes

--- Simulation/6DOF_RK4/forces.py
+++ Simulation/6DOF_RK4/forces.py
@@ -13,32 +13,34 @@ import numpy as np
 # Define Objects
 
 class Forces:
+    # Define class objects
     atm = atmosphere.Atmosphere()
-    motor = motor.Motor()
+    motor = motor.Motor() #TODO: Add paramters of motor being used
 
     def __init__(self):
         pass
 
-    def get_force(self, x_state, flap_ext, tStamp) -> float:
+    def get_force(self, x_state, flap_ext, tStamp) -> np.array(3):
+        # TODO: Add random disturbances
         return self.motor.get_thrust(tStamp) + self.aerodynamic_drag_force(x_state, flap_ext) + self.gravitational_force(x_state[0], tStamp) + self.wind_force()
 
     def get_Cd(flap_ext) -> float:
         # TODO: account for area change of flaps in C_d calculation
         return prop.C_d
     
-    def aerodynamic_drag_force(self, x_state, flap_ext) -> float:
+    def aerodynamic_drag_force(self, x_state, flap_ext) -> np.array(3):
         z = x_state[0]
         vel = x_state[1]
         v_mag = np.norm(vel)
         density = self.atm.get_density(z)
         return -(vel/v_mag) * 0.5*density*(v_mag**2)*self.get_Cd(flap_ext)*(prop.A)
     
-    def gravitational_force(self, altitude, tStamp) -> float:
+    def gravitational_force(self, altitude, tStamp) -> np.array(3):
         # F = GMm/r^2
         total_mass = prop.mass + self.motor.get_mass(tStamp) #Adding dry mass + motor mass
         return -np.array(0, [(prop.G*prop.m_e*total_mass)/((prop.r_e+altitude)**2)], 0)
     
-    def wind_force(self, altitude, tStamp) -> float:
+    def wind_force(self, altitude, tStamp) -> np.array(3):
         wind_vector = self.atm.get_wind_vector(tStamp)
         density = self.atm.get_density(altitude)
         wind_vector_mag = wind_vector.norm()
