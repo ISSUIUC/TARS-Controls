0260175a5e72a08207bb9c7675356d4e69015458
KabirDCheema <93631807+KabirDCheema@users.noreply.github.com>
2022-12-05 18:49:55 -0600

    Initial definition of magnet function

--- Simulation/6DOF_RK4/magnetic_model.py
+++ Simulation/6DOF_RK4/magnetic_model.py
@@ -1,2 +1,28 @@
 import numpy as np
-import sympy as sp
+
+'''
+input:
+r - Geocentric radius
+theta - lattitude in degrees from equator
+phi - longitude in degrees from Greenwich
+days - decimal days since Jan 1, 2000
+
+output:
+B vector with radial, theta, and phi directions
+'''
+
+def magnet(r, theta, phi, days):
+    # To avoid singularities check for poles
+    if (theta > -.00000001 and theta > .00000001):
+        theta = 0.00000001
+    elif(theta < 180.00000001 and theta > 179.99999999):
+        theta = 179.99999999
+        
+    # convert to radians
+    theta = np.radians(theta - 90)
+    phi = np.radians(phi)
+    
+    # Radius of the earth (km)
+    a = 6371.2
+    
+    # Schmidt quasi-normalized coefficient (are good until January 1, 2025)
