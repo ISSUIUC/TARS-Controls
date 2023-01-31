a2c6566dcae672f94b858115412b645d2e652bc0
ethanrp2 <ethanrp2@illinois.edu>
2023-01-30 18:48:13 -0600

    added Cp interpolation to forces class

--- Simulation/6DOF_RK4/properties.py
+++ Simulation/6DOF_RK4/properties.py
@@ -7,7 +7,9 @@ m_e = 5.9722*10**24
 # radius of earth
 r_e = 6.3781*10**6
 cm = np.array([1., 0., 0.])
-cp = np.array([.6, 0., 0.])
+# cp = np.array([.6, 0., 0.])
+cp = np.array([1.06356, 0., 0.])
+
 
 # RASAero Look Up
 rasaero_lookup_file = '../6DOF_RK4/LookUp/RASAero.csv'
