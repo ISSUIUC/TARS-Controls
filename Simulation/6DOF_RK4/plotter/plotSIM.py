cf23b06e4f8a251db22df539d109b1a0d076a110
Parth Shrotri <shrotri.parth@gmail.com>
2023-02-24 01:35:00 -0600

    new nice plots

--- Simulation/6DOF_RK4/plotter/plotSIM.py
+++ Simulation/6DOF_RK4/plotter/plotSIM.py
@@ -17,23 +17,23 @@ def plotter(sim_dict, sensor_dict=0, kalman_dict=0):
         sensor_dict (dict, optional): Dictionary containing sensor data. Defaults to 0.
         kalmann_dict (dict, optional): Dictionary containing kalman filter data. Defaults to 0.
     """
-    fig_linear,(pos_nc,vel_nc,accel_nc) = plt.subplots(3,1,figsize=(15,10), sharex=True)
-    fig_linear.suptitle("PYSIM 6DOF LINEAR PLOT", color='#F5B14C', fontsize = 25)
+    fig_linear,(pos_nc,vel_nc,accel_nc,flap_nc) = plt.subplots(4,1,figsize=(15,10), sharex=True)
+    fig_linear.suptitle("PYSIM 6DOF LINEAR PLOT", color='#F5B14C', fontsize = 20)
 
     # Altitude Measurements vs Real Altitude vs Kalman Filter Graph (No Control)
     plt.xlabel("Time (s)", fontsize = 14)
     pos_nc.plot(sim_dict["time"], sim_dict["pos"][:,0], label="X", color="tab:red", linewidth = 2)
     pos_nc.plot(sim_dict["time"], sim_dict["pos"][:,1], label="Y", color="tab:green", linewidth = 2)
     pos_nc.plot(sim_dict["time"], sim_dict["pos"][:,2], label="Z", color="tab:blue", linewidth = 2)
-    pos_nc.plot(sensor_dict["time"], sensor_dict["baro_alt"], label="Barometric Altimeter", color="darkred", linestyle = ":",linewidth = 2)
+    pos_nc.plot(sensor_dict["time"], sensor_dict["baro_alt"], label="Baro Alt", color="darkred", linestyle = ":",linewidth = 2)
     pos_nc.plot(kalman_dict["time"], kalman_dict["kalman_pos"][:,0], label="X Estimate", color="purple", linestyle = "dashed",linewidth = 2)
     pos_nc.plot(kalman_dict["time"], kalman_dict["kalman_pos"][:,1], label="Y Estimate", color="lime", linestyle = "dashed",linewidth = 2)
     pos_nc.plot(kalman_dict["time"], kalman_dict["kalman_pos"][:,2], label="Z Estimate", color="skyblue", linestyle = "dashed",linewidth = 2)
-    pos_nc.plot(sensor_dict["time"], sensor_dict["apogee_estimate"], label="Apogee Estimator",color="brown", linewidth = 2)
-    pos_nc.axhline(y=sim_dict["pos"][-1,0], label="Simulated Apogee", linestyle = "dashed", color="gray", linewidth = 2)
+    pos_nc.plot(sensor_dict["time"], sensor_dict["apogee_estimate"], label="Apogee Estimate",color="brown", linewidth = 2)
+    pos_nc.axhline(y=sim_dict["pos"][-1,0], label="Sim Apogee", linestyle = "dashed", color="gray", linewidth = 2)
     pos_nc.axhline(y=prop.des_apogee, label="Desired Apogee", linestyle = "dashed", color="orange", linewidth = 2)
-    pos_nc.set_ylabel("Position (m)")
-    pos_nc.legend(loc='best')
+    pos_nc.set_ylabel("Position (m)", fontsize = 10)
+    pos_nc.legend(fontsize=10, loc='upper left', ncol=3)
 
     # Real Velocity vs Kalman Filter Graph (No Control)
     vel_nc.plot(sim_dict["time"], sim_dict["vel"][:,0],label="X",color="tab:red", linewidth = 2);  
@@ -42,8 +42,8 @@ def plotter(sim_dict, sensor_dict=0, kalman_dict=0):
     vel_nc.plot(kalman_dict["time"], kalman_dict["kalman_vel"][:,0], label="X Estimate", color="purple", linestyle = "dashed",linewidth = 2)
     vel_nc.plot(kalman_dict["time"], kalman_dict["kalman_vel"][:,1], label="Y Estimate", color="lime", linestyle = "dashed",linewidth = 2)
     vel_nc.plot(kalman_dict["time"], kalman_dict["kalman_vel"][:,2], label="Z Estimate", color="skyblue", linestyle = "dashed",linewidth = 2)
-    vel_nc.set_ylabel("Velocity (m/s)")
-    vel_nc.legend(loc='best')
+    vel_nc.set_ylabel("Velocity (m/s)", fontsize = 10)
+    vel_nc.legend(fontsize=10, loc='upper left', ncol =2)
 
     # Acceleration Measurements vs Real Acceleration vs Kalman Filter Graph (No Control)
     accel_nc.plot(sim_dict["time"], sim_dict["accel"][:,0],label="X",color="tab:red", linewidth = 2);  
@@ -55,12 +55,17 @@ def plotter(sim_dict, sensor_dict=0, kalman_dict=0):
     accel_nc.plot(kalman_dict["time"], kalman_dict["kalman_accel"][:,0], label="X Estimate", color="purple", linestyle = "dashed",linewidth = 2)
     accel_nc.plot(kalman_dict["time"], kalman_dict["kalman_accel"][:,1], label="Y Estimate", color="lime", linestyle = "dashed",linewidth = 2)
     accel_nc.plot(kalman_dict["time"], kalman_dict["kalman_accel"][:,2], label="Z Estimate", color="skyblue", linestyle = "dashed",linewidth = 2)
-    accel_nc.set_ylabel("Acceleration (m/s^2)")
-    accel_nc.legend(loc='best')
+    accel_nc.set_ylabel("Acceleration (m/s^2)", fontsize = 10)
+    accel_nc.legend(fontsize=10, loc='upper left', ncol=3)
+
+    flap_nc.plot(sim_dict["time"], sim_dict["flap_ext"],label="Flap Extension",color="tab:green", linewidth = 2);   
+    flap_nc.set_ylabel("Flap Extension (m)", fontsize = 10);  
+    flap_nc.legend(fontsize=10, loc='upper left',ncol=1)
+
     plt.tight_layout()
 
     fig_angular,(ang_pos_nc, ang_vel_nc, ang_accel_nc, alpha_nc) = plt.subplots(4,1,figsize=(15,10), sharex=True)
-    fig_angular.suptitle("PYSIM 6DOF ANGULAR PLOT", color='#F5B14C', fontsize = 25)
+    fig_angular.suptitle("PYSIM 6DOF ANGULAR PLOT", color='#F5B14C', fontsize = 20)
 
     ang_pos_nc.plot(sim_dict["time"], sim_dict["ang_pos"][:,0], label="Roll", color="tab:red", linewidth = 2)
     ang_pos_nc.plot(sim_dict["time"], sim_dict["ang_pos"][:,1], label="Pitch", color="tab:green", linewidth = 2)
@@ -68,8 +73,8 @@ def plotter(sim_dict, sensor_dict=0, kalman_dict=0):
     ang_pos_nc.plot(sensor_dict["time"], sensor_dict["imu_ang_pos_x"], label="IMU Roll", color="darkred", linestyle = ":",linewidth = 2)
     ang_pos_nc.plot(sensor_dict["time"], sensor_dict["imu_ang_pos_y"], label="IMU Pitch", color="darkgreen", linestyle = ":",linewidth = 2)
     ang_pos_nc.plot(sensor_dict["time"], sensor_dict["imu_ang_pos_z"], label="IMU Yaw", color="darkblue", linestyle = ":",linewidth = 2)
-    ang_pos_nc.set_ylabel("Position (rad)");  
-    ang_pos_nc.legend(loc='best')
+    ang_pos_nc.set_ylabel("Position (rad)", fontsize = 14);  
+    ang_pos_nc.legend(fontsize=10, loc='upper left',ncol=2)
 
     ang_vel_nc.plot(sim_dict["time"], sim_dict["ang_vel"][:,0],label="Roll",color="tab:red", linewidth = 2);   
     ang_vel_nc.plot(sim_dict["time"], sim_dict["ang_vel"][:,1],label="Pitch",color="tab:green", linewidth = 2);  
@@ -77,25 +82,20 @@ def plotter(sim_dict, sensor_dict=0, kalman_dict=0):
     ang_vel_nc.plot(sensor_dict["time"], sensor_dict["imu_gyro_x"], label="IMU Roll Rate", color="darkred", linestyle = ":",linewidth = 2)
     ang_vel_nc.plot(sensor_dict["time"], sensor_dict["imu_gyro_y"], label="IMU Pitch Rate", color="darkgreen", linestyle = ":",linewidth = 2)
     ang_vel_nc.plot(sensor_dict["time"], sensor_dict["imu_gyro_z"], label="IMU Yaw Rate", color="darkblue", linestyle = ":",linewidth = 2)
-    ang_vel_nc.set_ylabel("Velocity (rad/s)");  
-    ang_vel_nc.legend(loc='best')
+    ang_vel_nc.set_ylabel("Velocity (rad/s)", fontsize = 14);  
+    ang_vel_nc.legend(fontsize=10, loc='upper left',ncol=2)
 
     ang_accel_nc.plot(sim_dict["time"], sim_dict["ang_accel"][:,0],label="Roll",color="tab:red", linewidth = 2);  
     ang_accel_nc.plot(sim_dict["time"], sim_dict["ang_accel"][:,1],label="Pitch",color="tab:green", linewidth = 2);  
     ang_accel_nc.plot(sim_dict["time"], sim_dict["ang_accel"][:,2],label="Yaw",color="tab:blue", linewidth = 2);    
-    ang_accel_nc.set_ylabel("Acceleration (rad\s^2)");   
-    ang_accel_nc.legend(loc='best')
+    ang_accel_nc.set_ylabel("Acceleration (rad\s^2)", fontsize = 14);   
+    ang_accel_nc.legend(fontsize=10, loc='upper left',ncol=1)
 
     alpha_nc.plot(sim_dict["time"], np.degrees(sim_dict["alpha"]),label="Alpha",color="tab:green", linewidth = 2);    
     alpha_nc.axhline(88 ,label="Alpha Limit",color="tab:red",linewidth = 2);  
-    alpha_nc.set_ylabel("Angle of Attack (degrees)");   
-    alpha_nc.legend(loc='best')
+    alpha_nc.set_ylabel("Angle of Attack (degrees)", fontsize = 14);   
+    alpha_nc.legend(fontsize=10, loc='center left',ncol=1)
     plt.tight_layout()
-
-    plt.figure()
-    plt.plot(sim_dict["time"], sim_dict["flap_ext"],label="Flap Extension",color="tab:green", linewidth = 2);   
-    plt.xlabel("Time (sec)"); 
-    plt.ylabel("Flap Extension (m)");  
 
     plt.show()
 
