5ccccb075974e2c167e40c658fb20bab699bc160
evandy2 <evandy2@illinois.edu>
2023-09-26 22:26:30 -0500

    Started adding staging to rocket class

--- Simulation/6DOF_RK4/dynamics/rocket.py
+++ Simulation/6DOF_RK4/dynamics/rocket.py
@@ -5,8 +5,11 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import environment.atmosphere as atmosphere
 import dynamics.motor as motor
+from motor import Motor
 import dynamics.forces as forces
+from rocket import Rocket
 
 class Rocket:
 
@@ -55,7 +58,7 @@ class Rocket:
     forces = None
     sim_config = None
     
-    def __init__(self, simulation_config, atm=None):
+    def __init__(self, simulation_config, atm:atmosphere.Atmosphere=None, stages:list[Rocket]=None):
         self.sim_config = simulation_config
         self.cm_rocket = simulation_config["rocket"]["cm"]
         self.cm_motor = simulation_config["motor"]["cm"]
@@ -73,6 +76,20 @@ class Rocket:
         self.A_s = 2 * self.r_r * self.l
         self.max_ext_length = simulation_config["flaps"]["max_ext_length"]
         self.atm = atm
+        
+        # Add stages to rocket via this list. Only the base rocket object should have stages, each stage should be its own rocket object with no stages
+        self.stages = stages
+        '''
+        Start with the base rocket object, which is the first stage, then increment at each separation event.
+        This is used to determine which stage the rocket is currently in. Start at -1, then increment to 0, 1, etc., so that
+        they align with the indices of the stages list
+        '''
+        self.current_stage = -1
+        '''
+        This is used to store the time of the latest separation event. Start at 0, then update at each separation event so that timestamp is
+        based on the latest separation event
+        '''
+        self.separation_timestamp = 0
         
         # need to change motor and forces constructors to refer to properties from this class rather than properties file
         self.motor = motor.Motor(self.rocket_dry_mass, 
@@ -94,16 +111,43 @@ class Rocket:
                                     simulation_config["rocket"]["rasaero_lookup_file"],
                                     self.atm)
 
-    def set_motor_mass(self, timestamp):
+    def set_motor_mass(self, timestamp) -> None:
         """Sets the mass of the motor at a given time
         
         Args:
             timestamp (float): Time in seconds
         """
-        self.motor_mass = self.motor.get_mass(timestamp)
+        if self.current_stage == -1:
+            self.motor_mass = self.motor.get_mass(timestamp)
+        else:
+            self.motor_mass = self.stages[self.current_stage].get_motor().get_mass(timestamp - self.separation_timestamp)
         self.rocket_total_mass = self.rocket_dry_mass + self.motor_mass
+    
+    def get_motor_mass(self, timestamp) -> float:
+        """Returns the mass of the motor at a given time
+            Only used for upper stages, don't use for base stage
+            use set_motor_mass for base stage
         
-    def I(self, total_mass=rocket_total_mass): 
+        Args:
+            timestamp (float): Time in seconds
+        
+        Returns:
+            float: Mass of the motor
+        """
+        return self.motor.get_mass(timestamp - self.separation_timestamp)
+    
+    def separate_stage(self, timestamp) -> None:
+        """Separates the current stage from the rocket and returns the next stage
+        """
+        self.current_stage += 1
+        self.separation_timestamp = timestamp
+    
+    def get_motor(self) -> Motor:
+        """Returns the motor of the rocket
+        """
+        return self.motor
+    
+    def I(self, total_mass=rocket_total_mass) -> np.ndarray: 
         """Returns the inertia matrix of the rocket
         
         Args:
@@ -117,7 +161,7 @@ class Rocket:
                                        (total_mass/12) * (self.l**2 + 3*self.r_r**2)])
 
     
-    def I_inv(self, total_mass=rocket_total_mass): 
+    def I_inv(self, total_mass=rocket_total_mass) -> np.ndarray: 
         """Returns the inverse of the inertia matrix of the rocket
         
         Args:
@@ -127,7 +171,7 @@ class Rocket:
                                        1/((total_mass/12) * (self.l**2 + 3*self.r_r**2)),
                                        1/((total_mass/12) * (self.l**2 + 3*self.r_r**2))])
     
-
+    
 # if __name__ == '__main__':
     # rocket = Rocket(motor_mass=5,rocket_dry_mass=2)
     # print(rocket.rocket_total_mass)
