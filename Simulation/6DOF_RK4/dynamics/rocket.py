2a92c88571381c89ec378fb68f985c3bf1dcef77
Dante Ratchev <104606374+RatchevD@users.noreply.github.com>
2024-01-19 17:22:43 -0600

    updates

--- Simulation/6DOF_RK4/dynamics/rocket.py
+++ Simulation/6DOF_RK4/dynamics/rocket.py
@@ -7,6 +7,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import dynamics.motor as motor
 import dynamics.forces as forces
+from estimation.navigation import Navigation
 
 class Rocket:
 
@@ -54,9 +55,10 @@ class Rocket:
     motor = None
     forces = None
     
-    def __init__(self, cm_rocket=np.array([3.34-1.86, 0., 0.]), cm_motor=np.array([0.3755, 0., 0.]),impulse=9671.0,
+    def __init__(self, ekf_args, rekf_args, cm_rocket=np.array([3.34-1.86, 0., 0.]), cm_motor=np.array([0.3755, 0., 0.]),impulse=9671.0,
                  motor_mass=8.064, delay=60, motor_lookup_file='../lookup/m2500.csv',rocket_dry_mass=14.691,r_r=0.0508,l=3.34,
                  max_ext_length=0.0178, atm=None):
+        self.Navigation = Navigation(*ekf_args,*rekf_args) 
         self.cm_rocket = cm_rocket
         self.cm_motor = cm_motor
 
