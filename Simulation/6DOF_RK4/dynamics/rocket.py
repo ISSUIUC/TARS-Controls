61b0e2055975837c27357470d11359770379a904
evandy2 <evandy2@illinois.edu>
2024-03-22 17:37:23 -0500

    small fixes
    Deleted some code that was commented out and some small formatting changes. Overall looks good

--- Simulation/6DOF_RK4/dynamics/rocket.py
+++ Simulation/6DOF_RK4/dynamics/rocket.py
@@ -67,7 +67,7 @@ class Rocket:
         self.motor_mass = stage_config["motor"]["motor_mass"]
         self.delay = stage_config["motor"]["delay"]
         self.motor_lookup_file = stage_config["motor"]["motor_lookup_file"]
-        self.Navigation = Navigation(dt, stage_config['sensors'],x0) 
+        self.Navigation = Navigation(dt, stage_config['sensors'], x0) 
 
         self.rocket_dry_mass = stage_config["rocket_body"]["dry_mass"]
         self.rocket_total_mass = self.rocket_dry_mass + self.motor_mass
--- Simulation/6DOF_RK4/simulation/pysim.py
+++ Simulation/6DOF_RK4/simulation/pysim.py
@@ -58,39 +58,6 @@ class Simulation:
         self.baro = 0
         self.time_stamp = time_stamp
         self.sensor_config = self.rocket.stage_config['sensors']
-    #    self.init_kalman_filters()
-
-    # def init_kalman_filters(self):
-    #     # TODO: Init with previous rocket data
-    #     self.kalman_filter = ekf.KalmanFilter(dt, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    #     # TODO: init this data with the previous rocket becuase of staging
-    #     x_data = []
-    #     y_data = []
-    #     z_data = []
-    #     for i in range(10):
-    #         reading = sensors.get_accelerometer_data(self.x, self.sensor_config)
-    #         x_data.append(reading[0])
-    #         y_data.append(reading[1])
-    #         z_data.append(reading[2])
-
-    #     accel_tracker = np.array([])
-
-    #     ax = sum(x_data)/len(x_data)
-    #     ay = sum(y_data)/len(y_data)
-    #     az = sum(z_data)/len(z_data)
-
-    #     pitch = -1 * (np.arctan2(-az,-ay) + np.pi/2)
-    #     yaw = np.arctan2(-ax,-ay) + np.pi/2
-    #     self.r_kalman_filter = r_ekf.KalmanFilter_R(dt, 0.0, 0.0, 0.0, pitch, 0.0, 0.0, yaw, 0.0, 0.0)
-    #     self.apogee_estimator = apg.Apogee(self.kalman_filter.get_state(), 0.1, 0.01, 3, 30, atm, self.rocket.stage_config)
-
-    # def update_kalman(self, baro_alt, accel, gyro, bno_ang_pos):
-    #     self.kalman_filter.priori()
-    #     self.kalman_filter.update(bno_ang_pos, baro_alt,
-    #                         accel[0], accel[1], accel[2])
-        
-    #     self.r_kalman_filter.priori()
-    #     self.r_kalman_filter.update(*gyro, *accel)
     
     def time_step(self):
         self.time_stamp += self.dt
@@ -99,7 +66,6 @@ class Simulation:
         while self.time_stamp < self.rocket.delay:
             baro_alt, accel, gyro, bno_ang_pos = self.get_sensor_data()
             self.rocket.Navigation.update_state(baro_alt, accel, gyro, bno_ang_pos)
-            # self.update_kalman(baro_alt, accel, gyro, bno_ang_pos)
             self.rocket.Navigation.kalman_filter.reset_lateral_pos()
             current_state, current_covariance, current_state_r = self.get_kalman_state()
 
