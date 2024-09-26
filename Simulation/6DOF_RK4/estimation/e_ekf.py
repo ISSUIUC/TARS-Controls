e975264b1e7ddf8d4a6d2a9ff3e9e890fccbc020
RJMilb <rjmilburn6@gmail.com>
2024-09-25 19:44:58 -0500

    created a new branch

---
+++ Simulation/6DOF_RK4/estimation/e_ekf.py
@@ -0,0 +1,10 @@
+# error state kalman fiter
+from filterpy.common import Q_continuous_white_noise
+import numpy as np
+import util.vectors as vct
+
+class ErrorStateKalmanFilter:
+    ""
+
+    """
+    def __init__(self):
