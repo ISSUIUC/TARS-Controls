7ec304df52bb62f4d8a392a80824913bfbae0114
14Million <kulasik2@illinois.edu>
2024-10-16 20:37:06 -0500

    Update altsim.py

--- Simulation/6DOF_RK4/plotter/altsim.py
+++ Simulation/6DOF_RK4/plotter/altsim.py
@@ -1,58 +1,84 @@
 # File for onbaording sim
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 
 
 class Sim:
-    def __init__(self, mass, thrust, cDrag, crossA, dm, dt, x0, temp0, pressure0):
+    def __init__(self, mass, thrust, cDrag, crossA, motorMass, dm, dt, x0, temp0, pressure0):
         self.mass = mass
         self.thrust = thrust
         self.cDrag = cDrag
         self.crossA = crossA
         self.dm = dm
         self.dt = dt
-        self.temp0 = temp0
+        self.temp0 = temp0 + 274.15
         self.pressure0 = pressure0
+        self.motorMass = motorMass
         self.time = 0
         self.x = x0
         self.v = 0
-        self.G = 6.6743 / np.power(10,11)
-        self.mEarth = 5.97219 * np.power(10,24)
-        self.rEarth = 6.378137 * np.power(10,6)
-        self.Fg = self.mass * self.G * self.mEarth / np.power(self.rEarth+self.x,2)
+        self.a = 0
+        self.G = 6.6743/(10**11)
+        self.mEarth = 5.97219*(10**24)
+        self.rEarth = 6.378137*(10**6)
         self.fNet = 0
 
     def position(self):
-        self.v = self.fNet*self.dt
+        self.a = self.fNet/self.mass
+        self.v += self.a*self.dt
         self.x += self.v*self.dt
         
     def aDrag(self):
         self.temp = self.temp0 - .0065*self.x
-        self.pressure = self.pressure0*((self.temp0/self.temp)**((9.81*.029)/(8.314*.0065)))
+        if(self.temp > 0):
+            self.pressure = self.pressure0*((self.temp0/self.temp)**((9.81*.029)/(8.314*.0065)))
+        else:
+            self.pressure = -self.pressure0*((self.temp0/(-self.temp))**((9.81*.029)/(8.314*.0065)))
         self.density = (.029*self.temp)/(8.314*self.pressure)
         self.drag = .5*self.density*self.v*self.v*self.cDrag*self.crossA
 
     def forceGravity(self):
-        self.mass += self.dm * self.dt
-        self.g = self.G * self.mEarth / (self.rEarth+self.x ** 2)
+        self.g = (self.G * self.mEarth)/((self.rEarth+self.x)**2)
         self.Fg = self.mass * self.g
 
+    def endStage(self):
+        if(self.motorMass <= 0):
+            self.thrust = 0
+            self.dm = 0
+        self.mass -= self.dm*self.dt
+        self.motorMass -= self.dm*self.dt
+
     def netForce(self):
-        self.fNet = self.thrust - self.drag - self.Fg
+        if(self.v >= 0):
+            self.fNet = self.thrust - self.drag - self.Fg
+        else:
+            self.fNet = self.drag - self.Fg
 
     def update(self):
-        self.position()
         self.aDrag()
         self.forceGravity()
+        self.endStage()
         self.netForce()
+        self.position()
+        self.time += self.dt
 
 if __name__ == '__main__':
-    start = np.array([3,2,1,"GO"])
-    for i in range(0,4):
-        print(start[i])
-        time.sleep(1)
-
-    launch = Sim(1, 6700, .75, 4, 5, .01, 0, 33, 102500)
-    while launch.position >= 0:
+    launch = Sim(50, 2000, .75, .25, 40, 1, .01, 0, 33, 102500)
+    x = [0]
+    t = [0]
+    drag = [0]
+    while launch.x >= 0:
         launch.update()
+        x.append(launch.x)
+        t.append(launch.time)
+        drag.append(launch.drag)
+    plt.plot(t, x)
+    plt.xlabel('time(s)')
+    plt.ylabel('height(m)')
+    plt.show()
+    # plt.plot(t, drag)
+    # plt.xlabel('time(s)')
+    # plt.ylabel('drag(N)')
+    # plt.show()
     
