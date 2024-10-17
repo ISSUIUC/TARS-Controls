cd9f9f956c3763953c122ebcaa77ecaf0b239019
Divij <diviaops@gmail.com>
2024-10-17 12:13:45 -0500

    plots created but data not showing

--- Simulation/6DOF_RK4/plotter/ekf_tester.py
+++ Simulation/6DOF_RK4/plotter/ekf_tester.py
@@ -23,23 +23,31 @@ kalman_dict = {"x": []}
 def readData () :
     global df_lowG_timestamp, df_highG_data, df_barometer_data
 
-    df = pandas.read_csv("../lookup/flight_computer_20221029.csv" ,
-        usecols= ['timestamp_ms', 'highg_ax', 'highg_ay', 'highg_az', 'barometer_altitude', 'state_est_x'])
+    df = pandas.read_csv("Simulation/6DOF_RK4/LookUp/data.csv" ,
+        usecols= ['timestamp', 'highg.ax', 'highg.ay', 'highg.az', 'barometer.altitude', 'orientation.roll','orientation.pitch','orientation.yaw'])
 
     return df.to_dict()
 
 
 def implementKF (measuredDict):
     global kalman_filter
+    print(4);
     # convert dict_values objects to lists so they're easier to work with
-    barometer_data = list(measuredDict["barometer_altitude"].values())
-    accel_x = list(measuredDict['highg_ax'].values())
-    accel_y = list(measuredDict['highg_ay'].values())
-    accel_z = list(measuredDict['highg_az'].values())
+    barometer_data = list(measuredDict["barometer.altitude"].values())
+    accel_x = list(measuredDict['highg.ax'].values())
+    accel_y = list(measuredDict['highg.ay'].values())
+    accel_z = list(measuredDict['highg.az'].values())
+    bno_attitude_roll = list(measuredDict['orientation.roll'].values())
+    bno_attitude_pitch = list(measuredDict['orientation.pitch'].values())
+    bno_attitude_yaw =list(measuredDict['orientation.yaw'].values())
+
     kalman_filter = kf.KalmanFilter(0.01, barometer_data[0], 0, accel_x[0]*9.8, 0, 0, accel_y[0]*9.8, 0, 0, accel_z[0]*9.8)
-    for x in range(len(barometer_data)):
+    # for x in range(len(barometer_data)):
+    # print(len(barometer_data))
+    for x in range(0,10000):
+        if x%1000 == 0: print(x)
         kalman_filter.priori()
-        kalman_filter.update(barometer_data[x], accel_x[x], accel_y[x]*-9.8, accel_z[x]*-9.8)
+        kalman_filter.update([bno_attitude_roll[x],bno_attitude_pitch[x],bno_attitude_yaw[x]],barometer_data[x], accel_x[x]*9.8, accel_y[x]*9.8, accel_z[x]*9.8)
         kalman_dict['x'].append(kalman_filter.get_state()[0])
 
 
@@ -47,13 +55,14 @@ def plotGraph (measuredDict):
     global df_lowG_timestamp, df_highG_data, df_barometer_data
     
     # convert the dict to a list of the values in each column so the data can be plotted
-    df_lowG_timestamp = measuredDict['timestamp_ms'].values()
-    df_barometer_data = measuredDict['barometer_altitude'].values()
-    df_flight_state_est = measuredDict['state_est_x'].values()
+    df_lowG_timestamp = list(measuredDict['timestamp'].values())[:10000]
+    # df_lowG_timestamp= df_lowG_timestamp[0:1000]
+    df_barometer_data = list(measuredDict['barometer.altitude'].values())[:10000]
+    # df_flight_state_est = measuredDict['state_est_x'].values()
 
     plt.plot(df_lowG_timestamp, df_barometer_data, label = "barometer data")
     plt.plot(df_lowG_timestamp, kalman_dict['x'], label = "state estimate")
-    plt.plot(df_lowG_timestamp, df_flight_state_est, label = "flight state estimate")
+    # plt.plot(df_lowG_timestamp, df_flight_state_est, label = "flight state estimate")
 
 
     plt.title('State estimate vs Measurement')
