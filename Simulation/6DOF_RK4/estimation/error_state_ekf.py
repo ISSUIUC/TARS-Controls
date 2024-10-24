812e91c2f5b03be8730e1776f5ed2d23674d0b10
Ishaan Kandamuri <ishaan.kandamuri@gmail.com>
2024-10-23 21:00:43 -0500

    WIP ESEKF: Update and priori need work, F matrix must be determined through Jacobian of f
    Co-authored-by: bargets <bargets@users.noreply.github.com>
    Co-authored-by: aneeshg5 <aneeshg5@illinois.edu>
    Co-authored-by: Dante Ratchev <RatchevD@users.noreply.github.com>
    Co-authored-by: Keshav B <kbalaji4@users.noreply.github.com>
    Co-authored-by: williamyeh1 <williamyeh1@users.noreply.github.com>

--- Simulation/6DOF_RK4/estimation/error_state_ekf.py
+++ Simulation/6DOF_RK4/estimation/error_state_ekf.py
@@ -3,83 +3,70 @@ from filterpy.common import Q_continuous_white_noise
 import numpy as np
 import util.vectors as vct
 
+
+# TODO: Figure out how to correctly implement 
 class ErrorStateKalmanFilter:
     """ Kalman Filter for sensor accuracy determination
     
     Args:
         dt (float): time step
-        pos_x (float): initial x position
-        vel_x (float): initial x velocity
-        accel_x (float): initial x acceleration
-        pos_y (float): initial y position
-        vel_y (float): initial y velocity
-        accel_y (float): initial y acceleration
-        pos_z (float): initial z position
-        vel_z (float): initial z velocity
-        accel_z (float): initial z acceleration
-    """
+        pos_x_err (float): initial x position error
+        vel_x_err (float): initial x velocity error
+        accel_x_err (float): initial x acceleration error
+        pos_y_err (float): initial y position error
+        vel_y_err (float): initial y velocity error
+        accel_y_err (float): initial y acceleration error
+        pos_z_err (float): initial z position error
+        vel_z_err (float): initial z velocity error     
+        accel_z_err (float): initial z acceleration error
 
-    def __init__(self, dt, pos_x_err, vel_x_err, accel_x_err, pos_y_err, vel_y_err, accel_y_err, pos_z_err, vel_z_err, accel_z_err):
+    """
+    
+    def _init_(self, dt, pos_x_err, pos_y_err, pos_z_err, vel_x_err, vel_y_err, vel_z_err, accel_x_err, accel_y_err, accel_z_err): 
+        
         self.dt = dt
+        
         self.x_k = np.zeros((9,1))
         self.Q = np.zeros((9,9))
         self.R = np.diag([2., 1.9, 1.9, 1.9])
         self.P_k = np.zeros((9,9))
-        self.x_priori = np.zeros((9,1))
+        self.x_priori = np.zeros((9,1)) 
         self.P_priori = np.zeros((9,9))
         self.F = np.zeros((9,9))
         self.H = np.zeros((4,9))
-
-        self.current_time = 0
+    
+        self.current_time = 0 
         self.s_dt = dt
         
-        self.x_k = np.array([pos_x_err, vel_x_err, accel_x_err, pos_y_err, vel_y_err, accel_y_err, pos_z_err, vel_z_err, accel_z_err]).T
-
+        self.x_k = np.array([pos_x_err, pos_y_err, pos_z_err, vel_x_err, vel_y_err, vel_z_err, accel_x_err, accel_y_err, accel_z_err]).T
+        
+        # Initialize F
         for i in range(3):
             self.F[3*i:3*i+3, 3*i:3*i+3] = [[1.0, dt, (dt**2) / 2],
                                             [0.0, 1.0, dt],
                                             [0.0, 0.0, 1.0]]
-            
-            self.Q[3*i:3*i+3, 3*i:3*i+3] = Q_continuous_white_noise(3, dt, 13.)
-
-
-        # accelerometer 3 axes
-        # barometric altimeter 1 axis
+        # Initialize Q and R covariance matrices 
+        self.Q[3*i:3*i+3, 3*i:3*i+3] = Q_continuous_white_noise(3, dt, 13.)
+        
         self.H = np.array([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                            [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                            [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
                            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
-
-    def priori(self):
-        """Sets priori state and covariance
+        
+        
+        def priori():
+            """ 
+            Sets priori state and covariance
             Try reading this: https://en.wikipedia.org/wiki/Kalman_filter#Details
             But basically predicts step
-        Args:
+            Args:
             u (float): control input
-        """
-        self.x_priori = self.F @ self.x_k
-        self.P_priori = (self.F @ self.P_k @ self.F.T) + self.Q
-
-    def update(self, bno_attitude, x_pos, x_accel, y_accel, z_accel):
-        """Updates state and covariance
+            """
+            # TODO: figure out if self.F has to be jacobian(f w/ respect to x) for ekf: f(x_{k-1})
+            self.x_priori = self.F @ self.x_k
+            self.P_priori = (self.F @ self.P_k @ self.F.T) + self.Q
         
-        Args:
-            bno_attitude (tuple): (roll, pitch, yaw) in radians
-            x_pos (float): x position
-            x_accel (float): x acceleration
-            y_accel (float): y acceleration
-            z_accel (float): z acceleration
-        """
-        K = (self.P_priori @ self.H.T) @ np.linalg.inv(self.H @ self.P_priori @ self.H.T + self.R)
-        acc = vct.body_to_world(*bno_attitude, np.array([x_accel, y_accel, z_accel])) + np.array([-9.81, 0, 0])
-        y_k = np.array([x_pos, *acc]).T
-    
-
-        self.x_k = self.x_priori + K @ (y_k - self.H @ self.x_priori)
-        self.P_k = (np.eye(len(K)) - K @ self.H) @ self.P_priori
-
-        self.current_time += self.s_dt
-
+        
     def get_state(self):
         """Returns current state
         
@@ -95,11 +82,8 @@ class ErrorStateKalmanFilter:
             np.array: current covariance
         """
         return np.diag(self.P_k)
-
+    
     def reset_lateral_pos(self):
         """Resets lateral position to 0
         """
         self.x_k[1:3] = [0,0]
-        
-
-
