b7ba7bf8d8a2482019d6400b4fd493e536bff6a1
Shishir <sb74@illinois.edu>
2025-01-21 20:55:25 -0600

    Co-authored-by: aneeshg5 <aneeshg5@illinois.edu> Co-authored-by: Shreyas Talluri <stallur10@users.noreply.github.com> Co-authored-by: Muhammad Arshad <MuhammadArshad123@users.noreply.github.com>

--- Simulation/6DOF_RK4/estimation/ekf.py
+++ Simulation/6DOF_RK4/estimation/ekf.py
@@ -18,7 +18,7 @@ class KalmanFilter:
         vel_z (float): initial z velocity
         accel_z (float): initial z acceleration
     """
-    def __init__(self, dt, pos_x, vel_x, pos_y, vel_y, pos_z, vel_z):
+    def __init__(self, dt, pos_x, vel_x, accel_x, pos_y, vel_y, accel_y, pos_z, vel_z, accel_z):
         self.dt = dt
         self.x_k = np.zeros((9,1))
         self.Q = np.zeros((9,9))
@@ -32,9 +32,7 @@ class KalmanFilter:
         self.current_time = 0
         self.s_dt = dt
 
-        # self.x_k = np.array([pos_x, vel_x, accel_x, pos_y, vel_y, accel_y, pos_z, vel_z, accel_z]).T
-        # assuming the angular position and vels are 0
-        self.x_k = np.array([pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, 0, 0, 0, 0, 0, 0]).T
+        self.x_k = np.array([pos_x, vel_x, accel_x, pos_y, vel_y, accel_y, pos_z, vel_z, accel_z]).T
 
         for i in range(3):
             # self.F[3*i:3*i+3, 3*i:3*i+3] = [[1.0, dt, (dt**2) / 2],
@@ -54,20 +52,24 @@ class KalmanFilter:
                            [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
                            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
 
-    def priori(self, R: np.ndarray, T:float, m:float, r:float, h:float, Cn:float, Ca:float, Cp:float, rho:float, roll:float, pitch:float, yaw:float):
+    def priori(self, R: np.ndarray, T:float, m:float, r:float, h:float, Cn:float, Ca:float, Cp:float, rho:float, bno_attitude:tuple, accel:tuple):
         """Sets priori state and covariance
             Try reading this: https://en.wikipedia.org/wiki/Kalman_filter#Details
             But basically predicts step
         Args:
             u (float): control input
         """
-        #calculate rotational velocity
-        pos_x, pos_y, pos_z = self.x_k[0:3]
-        vel_x, vel_y, vel_z = self.x_k[3:6]
-        vel_mag = np.linalg.norm(self.x_k[6:9])
-        w_x = (roll - self.x_k[9]) / self.dt  
-        w_y = (pitch - self.x_k[10]) / self.dt  
-        w_z = (yaw - self.x_k[11]) / self.dt          
+        # states tracked: x, vx, ax, y, vy, ay, z, vz, az
+        # pos in x_k is every third element starting from 0
+        
+        pos_x, pos_y, pos_z = self.x_k[0], self.x_k[3], self.x_k[6]
+        vel_x, vel_y, vel_z = self.x_k[1], self.x_k[4], self.x_k[7]
+        vel_mag = np.linalg.norm([vel_x, vel_y, vel_z])
+        acc_x, acc_y, acc_z = accel
+        w_acc = vct.body_to_world(*bno_attitude, np.array([acc_x, acc_y, acc_z])) + np.array([-9.81, 0, 0])
+
+        # approximate angular velocity
+        w_x, w_y, w_z = w_acc * self.s_dt
         J_x = 1/2 * m * r**2
         J_y = 1/3 * m * h**2 + 1/4 * m * r**2
         J_z = J_y     
@@ -77,21 +79,18 @@ class KalmanFilter:
         #TODO: Verify Cn is being pulled from Aneesh's lookup table
         Fay = 0.5*rho*(vel_mag**2)*Cn*(np.pi*r**2)
         Faz = Fay
-                
+
         g = 9.81 # Earth gravity
         Fg = np.array([-g, 0, 0])
         Fg_body = np.linalg.inv(R) @ Fg
         Fgx, Fgy, Fgz = Fg_body[0], Fg_body[1], Fg_body[2]      # gravitational forces expressed on the body in each direction
         Ftx, Fty, Ftz = T[0],T[1],T[2]                                # thrust forces in each direciton ( we assume that is in one direction)
-        
-        #TODO: This is for rotational ekf lowkey
-        # # we can do some trig to figure this out (it's in the textbook)
-        # # states tracked: x, vx, ax, y, vy, ay, z, vz, az
-        xdot = np.array([[vel_x], [(Fax + Ftx + Fgx) / m - (w_y*vel_z - w_z*vel_y)], [1.0],
-                 [vel_y], [(Fay + Fty + Fgy) / m - (w_z*vel_x - w_x*vel_y)], [1.0],
-                 [vel_z], [(Faz + Ftz + Fgz) / m - (w_x*vel_y - w_y*vel_x)], [1.0]
+        # states tracked: x, vx, ax, y, vy, ay, z, vz, az
+        xdot = np.array([vel_x, (Fax + Ftx + Fgx) / m - (w_y*vel_z - w_z*vel_y), 1.0,
+                 vel_y, (Fay + Fty + Fgy) / m - (w_z*vel_x - w_x*vel_y), 1.0,
+                 vel_z, (Faz + Ftz + Fgz) / m - (w_x*vel_y - w_y*vel_x), 1.0
                 ])
-        self.x_priori = self.x_k + xdot * self.dt
+        self.x_priori = self.x_k + xdot * self.s_dt
         
         # linearized dynamics are F
         self.F = np.array([[0, 0, 0, 1, 0, 0, 0, 0, 0], 
@@ -120,8 +119,7 @@ class KalmanFilter:
         K = (self.P_priori @ self.H.T) @ np.linalg.inv(self.H @ self.P_priori @ self.H.T + self.R)
         acc = vct.body_to_world(*bno_attitude, np.array([x_accel, y_accel, z_accel])) + np.array([-9.81, 0, 0])
         w_acc = vct.body_to_world(*bno_attitude, np.array([x_accel, y_accel, z_accel])) + np.array([-9.81, 0, 0])
-        
-        y_k = np.array([x_pos, *acc]).reshape(-1,1)
+        y_k = np.array([x_pos, *acc]).T
 
         self.x_k = self.x_priori + K @ (y_k - (self.H @ self.x_priori))
         self.P_k = (np.eye(len(K)) - K @ self.H) @ self.P_priori
@@ -147,8 +145,7 @@ class KalmanFilter:
     def reset_lateral_pos(self):
         """Resets lateral position to 0
         """
-        self.x_k[0, 1:3] = 0
-        # np.array([[0], [0]])
+        self.x_k[1:3] = [0, 0]
         
 
 
