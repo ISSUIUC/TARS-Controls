d917880e25b8c6fa690097150b41c767e24b9bad
asrikanth777 <as140@illinois.edu>
2025-01-29 23:21:56 -0600

    converted the tilt lockout changes over, now working on cutting second stage

--- Simulation/6DOF_RK4/dynamics/forces.py
+++ Simulation/6DOF_RK4/dynamics/forces.py
@@ -16,6 +16,8 @@ import random
 
 # Define Objects
 
+
+
 class Forces:
     """Forces on rocket:
     
@@ -36,7 +38,7 @@ class Forces:
     rasaero_file_location = "" # Will be set in constructor
     rasaero = None
 
-    def __init__(self, max_ext_length, cm, cp, A, A_s, rocket_dry_mass, motor, rasaero_lookup_file, atm):
+    def __init__(self, max_ext_length, cm, cp, A, A_s, rocket_dry_mass, motor, rasaero_lookup_file, atm, multiplier=1):
         self.max_ext_length = max_ext_length
         self.cm = cm
         self.cp = cp
@@ -45,12 +47,13 @@ class Forces:
         self.rocket_dry_mass = rocket_dry_mass
         self.motor = motor
         self.atm = atm
+        self.multiplier = multiplier
         self.rasaero_file_location = os.path.join(os.path.dirname(__file__), rasaero_lookup_file)
         self.rasaero = pd.read_csv(self.rasaero_file_location)
 
     
 
-    def get_force(self, x_state, flap_ext, time_stamp, ejection_force, theta, phi, density_noise=False) -> np.ndarray:
+    def get_force(self, x_state, flap_ext, time_stamp, ejection_force, theta, phi, multiplier, density_noise=False) -> np.ndarray:
         '''Calculates net force felt by rocket while accounting for thrust, drag, gravity, wind
 
         Args:
@@ -67,17 +70,15 @@ class Forces:
         density = self.atm.get_density(alt, noise=density_noise, position=x_state[0])
         thrust = self.motor.get_thrust(time_stamp)
         wind_vector = self.atm.get_wind_vector(time_stamp)
-        alpha = self.get_alpha(x_state, wind_vector)
+
+        """
+        if 
+
+
+        """
+
+        alpha = self.get_alpha(x_state, wind_vector) 
         drag = self.aerodynamic_force(x_state, density, wind_vector, alpha, self.rasaero, thrust.dot(thrust) > 0, flap_ext)
-
-
-        
-        # This is super hacky but i think there is some floating point bs going on
-        drag_magnitude = np.linalg.norm(drag)
-        max_drag = 70
-        if(drag_magnitude > max_drag):
-            drag = (drag/drag_magnitude) * max_drag
-
         grav = self.gravitational_force(alt, time_stamp)
         force = vct.body_to_world(*x_state[2],thrust + drag) + grav
         moment = vct.body_to_world(*x_state[2], np.cross(-self.cm, thrust) + self.aerodynamic_moment(drag))
@@ -208,10 +209,12 @@ class Forces:
         return aerodynamic_moment
         
     def get_alpha(self, x_state, wind_vector) -> float:
+
         incident_velocity = vct.world_to_body(*x_state[2], vct.norm(x_state[1] + wind_vector))
         orientation = vct.world_to_body(*x_state[2], np.array([1,0,0]))
         alpha = np.arccos(np.dot(incident_velocity, orientation))
         if(np.linalg.norm(incident_velocity) == 0):
             alpha = 0
-        return alpha
+        return alpha * self.multiplier
+    
     
